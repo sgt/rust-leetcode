f7aea6df093940f839f3d8daf95ef27572ac4313
Sergei Barbarash <sgt.sgt@gmail.com>
2022-09-18 19:26:14 +0300

    longest palindrome

--- src/lib.rs
+++ src/lib.rs
@@ -116,6 +116,32 @@ impl Solution {
         let len = linkedlist_len(&head);
         linkedlist_skip(head, len / 2)
     }
+
+    pub fn longest_palindrome(s: String) -> i32 {
+        let mut freq_map: HashMap<char, i32> = HashMap::new();
+        for c in s.chars() {
+            freq_map.insert(c, freq_map.get(&c).unwrap_or(&0) + 1);
+        }
+        let freqs: Vec<i32> = freq_map.into_values().collect();
+        let odd_exists: bool = freqs.iter().find(|x| *x % 2 == 1).map_or(false, |_| true);
+        let even_entries: i32 = freqs
+            .iter()
+            .filter_map(|x| {
+                if *x < 2 {
+                    None
+                } else if x % 2 == 1 {
+                    Some(x - 1)
+                } else {
+                    Some(*x)
+                }
+            })
+            .sum();
+        if odd_exists {
+            even_entries + 1
+        } else {
+            even_entries
+        }
+    }
 }
 
 #[cfg(test)]
@@ -200,5 +226,11 @@ mod tests {
                 1, 2, 3, 4, 5, 6
             ])))
         );
+    }
+
+    #[test]
+    fn test_longest_palindrome() {
+        assert_eq!(7, Solution::longest_palindrome("abccccdd".into()));
+        assert_eq!(1, Solution::longest_palindrome("a".into()));
     }
 }
