af6d3b45c746a14a36eb4b170ebd3952d2437e80
Sergei Barbarash <sgt.sgt@gmail.com>
2023-07-08 18:42:49 +0300

    still too slow

--- src/solution/s0018_4sum.rs
+++ src/solution/s0018_4sum.rs
@@ -1,3 +1,5 @@
+use std::collections::{BTreeSet, HashSet};
+
 pub struct Solution;
 
 impl Solution {
@@ -7,27 +9,46 @@ impl Solution {
     }
 
     pub fn k_sum(k: u32, nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
+        let mut nums = nums;
+        nums.sort();
+        Self::k_sum_rec(k, nums, target)
+    }
+
+    // TODO maybe return BTreeSet<BinaryHeap<i32>>
+    pub fn k_sum_rec(k: u32, nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
         match k {
             0 => vec![],
             1 => nums
                 .into_iter()
                 .filter_map(|x| (x == target).then(|| vec![x]))
                 .collect(),
+            2 => {
+                let mut result = BTreeSet::new();
+                let mut s = HashSet::new();
+                for n in nums {
+                    let rest = target - n;
+                    if s.contains(&rest) {
+                        result.insert(vec![rest, n]);
+                    } else {
+                        s.insert(n);
+                    }
+                }
+                result.into_iter().collect()
+            }
             _ => {
-                let mut result = vec![];
+                let mut result = BTreeSet::new();
                 for (i, n) in nums.iter().enumerate() {
                     let mut nums_without_n = nums.clone();
                     nums_without_n.swap_remove(i);
-                    let mut result_per_n = Self::k_sum(k - 1, nums_without_n, target - n);
+                    let mut result_per_n = Self::k_sum_rec(k - 1, nums_without_n, target - n);
                     for v in result_per_n.iter_mut() {
                         v.push(*n);
-                        v.sort();
+                        // TODO use BinaryHeap?
+                        v.sort_unstable();
                     }
-                    result.append(&mut result_per_n);
+                    result.append(&mut result_per_n.into_iter().collect());
                 }
-                result.sort();
-                result.dedup();
-                result
+                result.into_iter().collect()
             }
         }
     }
